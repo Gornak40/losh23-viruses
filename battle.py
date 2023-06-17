b89615f2580e663efd1580b3910112dd20c20831
Gornak40 <s-kozelsk@yandex.ru>
2023-06-17 03:42:50 +0300

    named logs

--- battle.py
+++ battle.py
@@ -1,17 +1,16 @@
-#!venv/bin/python
+#!venv/bin/python3
 import os
 import json
 from collections import defaultdict
-from prettytable import PrettyTable
 from termcolor import cprint
 from click import command, argument, option
 from click.types import Path
 
 
-def post(s, color=None):
+def post(s, field, color=None):
 	if color is not None:
 		cprint(s, color=color)
-	with open('story.log', 'a') as file:
+	with open(f'{field}_story.log', 'a') as file:
 		print(s, file=file)
 
 
@@ -23,33 +22,35 @@ def post(s, color=None):
 def battle(folder, field, no_compile, time_limit):
 	if not no_compile:
 		for name in filter(lambda x: x.endswith('.cpp'), os.listdir(folder)):
-			post(f'[compiling] {name}', color='yellow')
+			post(f'[compiling] {name}', field, color='yellow')
 			if os.system(f'g++ {os.path.join(folder, name)} -O3 -march=native -o {os.path.join(folder, name.removesuffix(".cpp"))}'):
-				post(f'[can\'t compile] {name}', color='red')
+				post(f'[can\'t compile] {name}', field, color='red')
 				exit(0)
-		post('[all binaries are ready]', color='green')
+		post('[all binaries are ready]', field, color='green')
 		return
 	names = sorted([x for x in os.listdir(folder) if not x.endswith('.cpp')], key=lambda x: x.title().split('_')[1:])
 	score = defaultdict(int)
 	for i, player1 in enumerate(names):
-		post(f'[running] {player1} #{i}', color='yellow')
+		if 'Егор' not in player1:
+			continue
+		post(f'[running] {player1} #{i}', field, color='yellow')
 		for player2 in names:
 			if player1 == player2:
 				continue
-			post(f'[fight] {player1} {player2}', color='cyan')
+			post(f'[fight] {player1} {player2}', field, color='cyan')
 			if os.system(f'python gorill.py {os.path.join(folder, player1)} {os.path.join(folder, player2)} val {field} -t {time_limit}'):
-				post(f'[fatal error]', color='red')
+				post(f'[fatal error]', field, color='red')
 				exit(0)
 			with open('result.out') as file:
 				if int(file.readline().strip()) == 1:
-					post('Player 1 wins')
+					post('Player 1 wins', field)
 					score[player1] += 1
 				else:
-					post('Player 2 wins')
+					post('Player 2 wins', field)
 					score[player2] += 1
-			with open('results.json', 'w', encoding='utf-8') as file:
+			with open(f'{field}_results.json', 'w', encoding='utf-8') as file:
 				json.dump(score, file, sort_keys=True, indent='\t', ensure_ascii=False)
-	post('Endgame')
+	post('Endgame', field)
 
 
 if __name__ == '__main__':
